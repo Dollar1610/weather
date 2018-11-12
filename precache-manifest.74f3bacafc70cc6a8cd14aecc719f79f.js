76fa2aba00cbb2749f6e76f8ce31d1432fd749aa
Виталий <Dollar16.10.91@mail.ru>
2018-11-12 12:33:53 +0500

    Updates

--- precache-manifest.74f3bacafc70cc6a8cd14aecc719f79f.js
+++ precache-manifest.74f3bacafc70cc6a8cd14aecc719f79f.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/weather/static/js/runtime~main.177e1b72.js"
   },
   {
-    "revision": "d987156b21f83c146dcb",
-    "url": "/weather/static/js/main.d987156b.chunk.js"
+    "revision": "6e849b98ebf84b5310ff",
+    "url": "/weather/static/js/main.6e849b98.chunk.js"
   },
   {
-    "revision": "9e625d5096eabdbaf360",
-    "url": "/weather/static/js/1.9e625d50.chunk.js"
+    "revision": "ea9d2b6a5490d21a7090",
+    "url": "/weather/static/js/1.ea9d2b6a.chunk.js"
   },
   {
-    "revision": "d987156b21f83c146dcb",
+    "revision": "6e849b98ebf84b5310ff",
     "url": "/weather/static/css/main.8df4069c.chunk.css"
   },
   {
-    "revision": "9e625d5096eabdbaf360",
+    "revision": "ea9d2b6a5490d21a7090",
     "url": "/weather/static/css/1.3563c346.chunk.css"
   },
   {
-    "revision": "dda991b7cb2c89ef3463a6c9cab8ad3c",
+    "revision": "395201398a1c53214fb2433e36e1b34e",
     "url": "/weather/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weather/precache-manifest.25147b014874751ab783fdababca59d4.js"
+  "/weather/precache-manifest.74f3bacafc70cc6a8cd14aecc719f79f.js"
 );
 
 workbox.clientsClaim();
