2619bb5a96820063bff3eb5497ba53ba8dd98fa0
Виталий <Dollar16.10.91@mail.ru>
2018-11-12 11:01:41 +0500

    Updates

---
+++ precache-manifest.2e63b61487a5bfed388cc1f3de4818a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b0a9000967fb9c7f0c6",
+    "url": "/weather-app-v1/static/js/runtime~main.3b0a9000.js"
+  },
+  {
+    "revision": "4a3fc06dcc59e5f34771",
+    "url": "/weather-app-v1/static/js/main.4a3fc06d.chunk.js"
+  },
+  {
+    "revision": "9e625d5096eabdbaf360",
+    "url": "/weather-app-v1/static/js/1.9e625d50.chunk.js"
+  },
+  {
+    "revision": "4a3fc06dcc59e5f34771",
+    "url": "/weather-app-v1/static/css/main.135d775b.chunk.css"
+  },
+  {
+    "revision": "9e625d5096eabdbaf360",
+    "url": "/weather-app-v1/static/css/1.3563c346.chunk.css"
+  },
+  {
+    "revision": "30b065deb907ec6e49cfa51f8329d266",
+    "url": "/weather-app-v1/index.html"
+  }
+];
